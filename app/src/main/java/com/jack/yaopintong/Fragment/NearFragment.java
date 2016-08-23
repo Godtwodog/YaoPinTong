8a260313e75b669926a9aa44d7c5392611df6298
deng xiaosheng <xiaosheng@gmail.com>
2016-08-23 20:33:01 +0800

    邓晓升第三次更新

--- app/src/main/java/com/jack/yaopintong/Fragment/NearFragment.java
+++ app/src/main/java/com/jack/yaopintong/Fragment/NearFragment.java
@@ -1,84 +1,52 @@
 package com.jack.yaopintong.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.MapView;
+import com.jack.yaopintong.Activity.MapActivity;
 import com.jack.yaopintong.R;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 
 /**
  * Created by JackCheung on 2016/8/19.
  * 秘钥：NeGBy4tzbQn4TLFCmncX9vRaNHGqWTA5
  */
 public class NearFragment extends Fragment {
-    @Bind(R.id.toobar_near)
-    Toolbar toobarNear;
-    @Bind(R.id.bmapView)
-    MapView mMapView;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 0:
+                    Intent intent=new Intent();
+                    intent.setClass(getActivity(),MapActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        handler.sendEmptyMessageDelayed(0,1000);
     }
 
-    /**
-     *
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return
-     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
-        //注意该方法要再setContentView方法之前实现
-        SDKInitializer.initialize(getActivity().getApplicationContext());
         View view = inflater.inflate(R.layout.fragment_near, container, false);
-        ButterKnife.bind(this, view);
         return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.unbind(this);
-    }
 }
